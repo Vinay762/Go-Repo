7e5039610ffee0a411512c8c3b0170cca781df0f
= <vinaytiwari7503334418@gmail.com>
2023-04-06 02:52:32 +0530

    Maps

---
+++ Maps/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Maps in Golang")
+
+	languages := make(map[string]string)
+
+	languages["JS"] = "Javascript"
+	languages["RB"] = "Ruby"
+	languages["Py"] = "Python"
+
+	fmt.Println("List of all languages ", languages)
+	fmt.Println("Js is short of ", languages["JS"])
+
+	delete(languages, "RB")
+	fmt.Println("List of all languages ", languages)
+
+	//loops are interestig in golang;
+	for key, value := range languages {
+		fmt.Printf("For key %v, value is %v\n", key, value)
+	}
+}
